f1c42e6ff6c5cbde99a54fa4c680c63ae5fcda36
Ethan McCue <ethan@mccue.dev>
2022-11-07 16:27:27 -0500

    Pairing changes

--- src/main/java/dev/mccue/resolve/maven/MavenCentralDownloader.java
+++ src/main/java/dev/mccue/resolve/maven/MavenCentralDownloader.java
@@ -1,5 +1,10 @@
 package dev.mccue.resolve.maven;
 
+import dev.mccue.resolve.core.Classifier;
+import dev.mccue.resolve.core.Extension;
+import dev.mccue.resolve.core.ModuleName;
+import dev.mccue.resolve.core.Organization;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.*;
@@ -8,8 +13,11 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * Creates an object from a specified path at maven central. Allows for the downloading of POMS and Jars.
@@ -17,75 +25,43 @@ import java.time.Duration;
 public final class MavenCentralDownloader {
 
     private static final String MAVEN_BASE_URL = "https://repo.maven.apache.org/maven2/";
-    private static final String POM_EXTENSION = ".pom";
-    private static final String JAR_EXTENSION = ".jar";
 
     private final String dependencyBaseURL;
-    private final String group;
-    private final String name;
+    private final Organization organization;
+    private final ModuleName moduleName;
     private final String version;
-    private final String path;
 
     /**
      * Constructor for dev.mccue.resolve.maven.MavenCentralDownloader
      * Checks the connection for successful 200 response
      *
-     * @param group   the name of dependencies' group
-     * @param name    the name of the dependency
-     * @param version the version of the dependency
      */
-    public MavenCentralDownloader(String group, String name, String version, String relativePath) throws Exception {
-        this.group = group;
-        this.name = name;
+    public MavenCentralDownloader(Organization organization, ModuleName moduleName, String version) throws Exception {
+        this.organization = organization;
+        this.moduleName = moduleName;
         this.version = version;
-        this.path = relativePath;
-        this.dependencyBaseURL = MAVEN_BASE_URL +
-                URLEncoder.encode(group, StandardCharsets.UTF_8) + "/" +
-                URLEncoder.encode(name, StandardCharsets.UTF_8) + "/" +
-                URLEncoder.encode(version, StandardCharsets.UTF_8) + "/";
 
-        var url = new URL(this.dependencyBaseURL);
-        var conn = (HttpURLConnection) url.openConnection();
-        conn.disconnect();
-        if (conn.getResponseCode() != 200) {
-            throw new Exception("Bad Response Code: " + conn.getResponseCode());
-        }
+        var organizationUrlFragment = Arrays.stream(organization.value().split("\\."))
+                .map(part -> URLEncoder.encode(part, StandardCharsets.UTF_8))
+                .collect(Collectors.joining("/"));
+
+        this.dependencyBaseURL = MAVEN_BASE_URL +
+                organizationUrlFragment + "/" +
+                URLEncoder.encode(moduleName.value(), StandardCharsets.UTF_8) + "/" +
+                URLEncoder.encode(version, StandardCharsets.UTF_8) + "/";
     }
 
     /**
      * Downloads the POM file locally and stores it at the specified path
-     *
-     * @throws IOException
      */
-    public void getPOM() throws IOException, InterruptedException {
-        Files.createDirectories(Paths.get(this.path));
-        var fileName = name + "-" + version + POM_EXTENSION;
-        var client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
-                .followRedirects(HttpClient.Redirect.NORMAL)
-                .connectTimeout(Duration.ofSeconds(20))
-                .build();
-        var request = HttpRequest.newBuilder()
-                .uri(URI.create(this.dependencyBaseURL + fileName))
-                .build();
-        var response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
-        if (response.statusCode() == 200) {
-            try (var outputStream = new FileOutputStream(this.path + fileName + ".xml")) {
-                outputStream.write(response.body());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    public void get(Extension extension, Classifier classifier, Path path) throws IOException, InterruptedException {
+        Files.createDirectories(path);
+        var fileName = moduleName.value()
+                + "-"
+                + version
+                + (classifier.isEmpty() ? "" : "-" + classifier.value() )
+                + (extension.isEmpty() ? "" : "." + extension.value());
 
-    /**
-     * Downloads the Jar file locally and stores it at the specified path
-     *
-     * @throws IOException
-     */
-    public void getJar() throws IOException, InterruptedException {
-        Files.createDirectories(Paths.get(this.path));
-        var fileName = name + "-" + version + JAR_EXTENSION;
         var client = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .followRedirects(HttpClient.Redirect.NORMAL)
@@ -94,20 +70,31 @@ public final class MavenCentralDownloader {
         var request = HttpRequest.newBuilder()
                 .uri(URI.create(this.dependencyBaseURL + fileName))
                 .build();
-        var response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
-        if (response.statusCode() == 200) {
-            try (var outputStream = new FileOutputStream(this.path + fileName)) {
-                outputStream.write(response.body());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        var response = client.send(request, HttpResponse.BodyHandlers.ofFile(
+                Path.of( path.toString(), fileName)
+        ));
+
+        if (response.statusCode() != 200) {
+            throw new IOException("Bad response code: " + response.statusCode());
         }
     }
 
     public static void main(String[] args) throws Exception {
-        var u = new MavenCentralDownloader("HTTPClient", "HTTPClient", "0.3-3", "./downloads/http/");
-        u.getPOM();
-        u.getJar();
+        var u = new MavenCentralDownloader(
+                new Organization("HTTPClient"),
+                new ModuleName("HTTPClient"),
+                "0.3-3"
+        );
+        u.get(Extension.POM, Classifier.EMPTY, Path.of("./downloads/http/"));
+        u.get(Extension.JAR, Classifier.EMPTY, Path.of("./downloads/http/"));
+
+        var u2 = new MavenCentralDownloader(
+                new Organization("com.agile4j"),
+                new ModuleName("agile4j-feed-builder"),
+                "1.0.3"
+        );
+
+        u2.get(Extension.JAR, Classifier.JAVADOC, Path.of("./downloads/http/"));
     }
 
 }
