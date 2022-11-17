ca2edc098e4f3c107564d0dd719fda5a96da0d39
zks1596_evt <zks1596@rit.edu>
2022-11-17 16:21:57 -0500

    Added some file locking functions. Still need more before this can be considered done

--- src/main/java/dev/mccue/resolve/core/CacheHandler.java
+++ src/main/java/dev/mccue/resolve/core/CacheHandler.java
@@ -1,84 +1,193 @@
 package dev.mccue.resolve.core;
 
-import java.io.FileOutputStream;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.channels.Channels;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.*;
 import java.util.Locale;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 
 public final class CacheHandler {
     public enum OSLabel {
-        Linux, MacOS, Windows
+        LINUX, MACOS, WINDOWS
     }
-    private static OSLabel currentOS;
-    private static String CACHE_PATH;
+    private static final OSLabel CURRENT_OS;
+    private static final String CACHE_PATH;
 
-    public CacheHandler() {
+    static {
         /* This only handles the default cache path and will need to
         *  be modified when new features are implemented (e.g. CLI --cache option)
         */
-        checkOS();
-        switch (currentOS){
-            case Linux:
-                CACHE_PATH = System.getProperty("user.home") + "/.cache/jresolve";
-                break;
-            case MacOS:
-                CACHE_PATH = System.getProperty("user.home") + "/Library/Caches/JResolve";
-                break;
-            case Windows:
-                CACHE_PATH = System.getenv("APPDATA") + "\\JResolve\\Cache";
-                break;
-            case null:
+        String operatingSystem = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+        OSLabel currentOS = null;
+        if (operatingSystem.contains("nux")) {
+            currentOS = OSLabel.LINUX;
+        } else if (operatingSystem.contains("mac")) {
+            currentOS = OSLabel.MACOS;
+        } else if (operatingSystem.contains("win")) {
+            currentOS = OSLabel.WINDOWS;
+        }
+        CURRENT_OS = currentOS;
+
+        CACHE_PATH = switch (currentOS){
+            case LINUX ->
+                System.getProperty("user.home") + "/.cache/jresolve";
+            case MACOS ->
+                System.getProperty("user.home") + "/Library/Caches/JResolve";
+            case WINDOWS ->
+                System.getenv("APPDATA") + "\\JResolve\\Cache";
+            case null ->
                 throw new IllegalArgumentException("Operating System could not be determined.");
-            case default:
-                break;
-        }
+        };
     }
 
-    public void cacheAdd(String url) throws Exception {
-        String[] parsedURL = URLParser(url);
-        StringBuilder path;
-        if (currentOS != OSLabel.Windows) {
-            path = new StringBuilder(CACHE_PATH + "/" + parsedURL[0] + "/");
-            for(int i = 1; i < parsedURL.length-1; i++) {
-                path.append(parsedURL[i]).append("/");
-            }
-        } else {
-            path = new StringBuilder(CACHE_PATH + "\\" + parsedURL[0] + "\\");
-            for(int i = 1; i < parsedURL.length-1; i++) {
-                path.append(parsedURL[i]).append("\\");
-            }
-        }
-        Files.createDirectories(Paths.get(path.toString()));
-
-        URL downloadURL = new URL(url);
-        try ( var readableByteChannel = Channels.newChannel(downloadURL.openStream());
-              var fileOutputStream = new FileOutputStream(path + parsedURL[parsedURL.length-1]);
-        ) {
-            fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-        }
-    }
-
-    public String[] URLParser(String url) {
+    public String[] URLSplitter(String url) {
         return url.split("(://|/)+", -1);
     }
 
-    public static void checkOS() {
-        if (currentOS == null) {
-            String operatingSystem = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
-            if (operatingSystem.contains("nux")) {
-                currentOS = OSLabel.Linux;
-            } else if (operatingSystem.contains("mac")) {
-                currentOS = OSLabel.MacOS;
-            } else if (operatingSystem.contains("win")) {
-                currentOS = OSLabel.Windows;
+    public static File localFile(String url, File currCache, String username, boolean needsCached) throws MalformedURLException, URISyntaxException {
+        if (url.startsWith("file:/") && !needsCached) {
+            try {
+                return Paths.get(new URI(url)).toFile();
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String[] urlSearch = url.split(":", 2);
+        if (urlSearch.length < 2) {
+            throw new MalformedURLException("Missing protocol in: " + url);
+        }
+
+        String pre = urlSearch[0];
+        String post = urlSearch[1];
+        String rest = "";
+
+        if (post.startsWith("///")) {
+            rest = post.substring(3);
+        } else if (post.startsWith("/")) {
+            rest = post.substring(1);
+        } else {
+            throw new MalformedURLException("Missing absolute path in: " + url);
+        }
+
+        while (rest.startsWith("/")) {
+            rest = rest.substring(1);
+        }
+
+        String user = "";
+        if (username != null) {
+            user = username + "@";
+        }
+
+        URI finalURI = new URI(pre, "/", user, rest);
+        return new File(currCache, finalURI.toASCIIString());
+    }
+
+    public static Path lockPath(Path path) {
+        return path.getParent().resolve(path.getFileName().toString() + ".lock");
+    }
+
+    public static File lockFile(File file) {
+        return new File(file.getParentFile(), file.getName() + ".lock");
+    }
+
+    public static File defaultCache() throws IOException {
+        return JResolvePaths.cacheDirectory();
+    }
+
+    private static final ConcurrentHashMap<String, Object> cacheStringInterns = new ConcurrentHashMap<>();
+    private static Object lockWith(Path path) {
+        String key = "jresolve-lock-" + path.toString();
+        if (cacheStringInterns.contains(key)) {
+            return cacheStringInterns.get(key);
+        } else {
+            String newIntern = key.intern();
+            cacheStringInterns.putIfAbsent(newIntern, newIntern);
+            return newIntern;
+        }
+    }
+
+    public static <V> V structurePathLock(Path path, Callable<V> call) throws Exception {
+        Object procLock = lockPath(path);
+
+        synchronized (procLock) {
+            Path locked = path.resolve(".structure.lock");
+            try {
+                Files.createDirectories(path);
+            } catch (FileAlreadyExistsException e) {
+                if (!Files.isDirectory(path)) {
+                    throw e;
+                }
+            }
+            FileChannel chan = null;
+            
+            try {
+                chan = FileChannel.open( locked, StandardOpenOption.CREATE,
+                        StandardOpenOption.WRITE, StandardOpenOption.DELETE_ON_CLOSE);
+
+                FileLock lock = null;
+                try {
+                    lock = chan.lock();
+                    
+                    try {
+                        return call.call();
+                    } finally {
+                        lock.release();
+                        chan.close();
+                    }
+                } finally {
+                    if (lock != null) {
+                        lock.release();
+                    }
+                }
+            } finally {
+                if (chan != null) {
+                    chan.close();
+                }
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        CacheHandler cache = new CacheHandler();
-        cache.cacheAdd("https://repo1.maven.org/maven2/org/scala-lang/scala-library/2.12.7/scala-library-2.12.7.jar");
+    public static <V> V structureFileLock (File file, Callable<V> call) throws Exception {
+        Path filePath = file.toPath();
+        Object procLock = lockPath(filePath);
+
+        synchronized (procLock) {
+            File locked = new File(file, ".structure.lock");
+            try {
+                Files.createDirectories(filePath.getParent());
+            } catch (FileAlreadyExistsException e) {
+                if (!Files.isDirectory(filePath.getParent())) {
+                    throw e;
+                }
+            }
+
+            try (FileOutputStream stream = new FileOutputStream(locked)) {
+
+                FileLock lock = null;
+                try {
+                    lock = stream.getChannel().lock();
+
+                    try {
+                        return call.call();
+                    } finally {
+                        lock.release();
+                        stream.close();
+                        locked.delete();
+                    }
+                } finally {
+                    if (lock != null) {
+                        lock.release();
+                    }
+                }
+            }
+        }
     }
 }
