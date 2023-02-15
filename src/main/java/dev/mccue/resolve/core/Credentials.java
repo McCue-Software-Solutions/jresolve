f59493928ec6bb137715079d5d9681433f379d7e
Nathan Petrangelo <nathan@petrangelo.org>
2023-02-15 15:17:43 -0500

    Removed Serializable (can add it back if necessary in the future)

--- src/main/java/dev/mccue/resolve/core/Credentials.java
+++ src/main/java/dev/mccue/resolve/core/Credentials.java
@@ -1,8 +1,6 @@
 package dev.mccue.resolve.core;
 
-import java.io.Serializable;
-
-public record Credentials(String user, String password) implements Serializable {
+public record Credentials(String user, String password) {
     @Override
     public String toString() {
         return "Credentials[user=" + user + ", password=***]";
