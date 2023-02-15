adcbdcaf0e88bde0a06ff843d2f2220ea5f4ae5b
zsteig <steigerwald.zachary@gmail.com>
2023-02-15 16:56:43 -0500

    Added outline for soon-to-be-implemented features like cacheArchive as well as started the base functionality of file locking

--- src/main/java/dev/mccue/resolve/cache/CacheHandler.java
+++ src/main/java/dev/mccue/resolve/cache/CacheHandler.java
@@ -1,51 +1,32 @@
-package dev.mccue.resolve.core;
+package dev.mccue.resolve.cache;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Locale;
+import dev.mccue.resolve.cache.CacheDefaults.OSLabel;
 
 public final class CacheHandler {
-    public enum OSLabel {
-        Linux, MacOS, Windows
-    }
     private static OSLabel currentOS;
-    private static String CACHE_PATH;
-
-    public CacheHandler() {
-        /* This only handles the default cache path and will need to
-        *  be modified when new features are implemented (e.g. CLI --cache option)
-        */
-        checkOS();
-        switch (currentOS){
-            case Linux:
-                CACHE_PATH = System.getProperty("user.home") + "/.cache/jresolve";
-                break;
-            case MacOS:
-                CACHE_PATH = System.getProperty("user.home") + "/Library/Caches/JResolve";
-                break;
-            case Windows:
-                CACHE_PATH = System.getenv("APPDATA") + "\\JResolve\\Cache";
-                break;
-            case null:
-                throw new IllegalArgumentException("Operating System could not be determined.");
-            case default:
-                break;
-        }
-    }
+    private static String cacheLocation;
 
     public void cacheAdd(String url) throws Exception {
+        new CacheDefaults(checkOS());
+        cacheLocation = CacheDefaults.cacheLocation;
+
         String[] parsedURL = URLParser(url);
         StringBuilder path;
         if (currentOS != OSLabel.Windows) {
-            path = new StringBuilder(CACHE_PATH + "/" + parsedURL[0] + "/");
+            path = new StringBuilder(cacheLocation + "/" + parsedURL[0] + "/");
             for(int i = 1; i < parsedURL.length-1; i++) {
                 path.append(parsedURL[i]).append("/");
             }
         } else {
-            path = new StringBuilder(CACHE_PATH + "\\" + parsedURL[0] + "\\");
+            path = new StringBuilder(cacheLocation + "\\" + parsedURL[0] + "\\");
             for(int i = 1; i < parsedURL.length-1; i++) {
                 path.append(parsedURL[i]).append("\\");
             }
@@ -60,23 +41,27 @@ public final class CacheHandler {
         }
     }
 
+    public static OSLabel checkOS() {
+        if (currentOS == null) {
+            String operatingSystem = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+            if (operatingSystem.contains("nux")) {
+                currentOS = CacheDefaults.OSLabel.Linux;
+            } else if (operatingSystem.contains("mac")) {
+                currentOS = CacheDefaults.OSLabel.MacOS;
+            } else if (operatingSystem.contains("win")) {
+                currentOS = CacheDefaults.OSLabel.Windows;
+            }
+        }
+
+        return currentOS;
+    }
+
+    //Not really needed anymore
     public String[] URLParser(String url) {
         return url.split("(://|/)+", -1);
     }
 
-    public static void checkOS() {
-        if (currentOS == null) {
-            String operatingSystem = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
-            if (operatingSystem.contains("nux")) {
-                currentOS = OSLabel.Linux;
-            } else if (operatingSystem.contains("mac")) {
-                currentOS = OSLabel.MacOS;
-            } else if (operatingSystem.contains("win")) {
-                currentOS = OSLabel.Windows;
-            }
-        }
-    }
-
+    //Simply to test loading functionality
     public static void main(String[] args) throws Exception {
         CacheHandler cache = new CacheHandler();
         cache.cacheAdd("https://repo1.maven.org/maven2/org/scala-lang/scala-library/2.12.7/scala-library-2.12.7.jar");
