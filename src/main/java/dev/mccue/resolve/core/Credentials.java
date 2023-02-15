ca67a72f7dd48fff180cb37583ac4299166465f6
Nathan Petrangelo <nathan@petrangelo.org>
2023-02-15 15:06:14 -0500

    Removed unnecessary toString() and hashCode() methods

--- src/main/java/dev/mccue/resolve/core/Credentials.java
+++ src/main/java/dev/mccue/resolve/core/Credentials.java
@@ -4,12 +4,7 @@ import java.io.Serializable;
 
 public record Credentials(String user, String password) implements Serializable {
     @Override
-    public int hashCode() {
-        return 37 * (17 + user.hashCode()) + password.hashCode();
-    }
-
-    @Override
     public String toString() {
-        return "Credentials(" + user + ", ****)";
+        return "Credentials[user=" + user + ", password=***]";
     }
 }
