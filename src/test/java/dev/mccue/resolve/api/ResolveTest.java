d139b6c82bfa86120a594fa1c16ea80d0c69e623
Wald <58920237+zsteig@users.noreply.github.com>
2023-03-29 14:24:10 -0400

    Started to test fake POMs

--- src/test/java/dev/mccue/resolve/api/ResolveTest.java
+++ src/test/java/dev/mccue/resolve/api/ResolveTest.java
@@ -23,4 +23,69 @@ public class ResolveTest {
                 new Dependency("org.clojure", "spec.alpha", "0.3.218")
             ), mock.downloaded);
     }
+
+    @Test
+    public void testResolveUnrelatedDeps() throws SAXException, ModelParseException {
+        var mock = new MockRepository("graph1");
+        var r = new Resolve(mock)
+                .addDependency(new Dependency("jresolve.test", "first.solo.dep", "1.0.0"))
+                .addDependency(new Dependency("jresolve.test", "second.solo.dep", "1.1.1"));
+        r.run();
+        assertEquals(Set.of(
+                new Dependency("jresolve.test", "first.solo.dep", "1.0.0"),
+                new Dependency("jresolve.test", "second.solo.dep", "1.1.1")
+        ), mock.downloaded);
+    }
+
+    @Test
+    public void testResolveMatchingDeps() throws SAXException, ModelParseException {
+        var mock = new MockRepository("graph1");
+        var r = new Resolve(mock)
+                .addDependency(new Dependency("jresolve.test", "first.solo.dep", "1.0.0"))
+                .addDependency(new Dependency("jresolve.test", "first.solo.dep", "1.0.0"));
+        r.run();
+        assertEquals(Set.of(
+                new Dependency("jresolve.test", "first.solo.dep", "1.0.0")
+        ), mock.downloaded);
+    }
+
+    @Test
+    public void testResolveConflictingVersions() throws SAXException, ModelParseException {
+        var mock = new MockRepository("graph1");
+        var r = new Resolve(mock)
+                .addDependency(new Dependency("jresolve.test", "first.solo.dep", "1.0.0"))
+                .addDependency(new Dependency("jresolve.test", "first.solo.dep", "1.2.0"));
+        r.run();
+        assertEquals(Set.of(
+                new Dependency("jresolve.test", "first.solo.dep", "1.2.0")
+        ), mock.downloaded);
+    }
+
+    @Test
+    public void testResolveMatchingSubDeps() throws SAXException, ModelParseException {
+        var mock = new MockRepository("graph1");
+        var r = new Resolve(mock)
+                .addDependency(new Dependency("jresolve.test", "first.parent.of.new.dep", "1.12.3"))
+                .addDependency(new Dependency("jresolve.test", "second.parent.of.new.dep", "1.10.2"));
+        r.run();
+        assertEquals(Set.of(
+                new Dependency("jresolve.test", "first.parent.of.new.dep", "1.12.3"),
+                new Dependency("jresolve.test", "second.parent.of.new.dep", "1.10.2"),
+                new Dependency("jresolve.test", "child.new.dep", "3.4.5")
+        ), mock.downloaded);
+    }
+
+    @Test
+    public void testResolveConflictingVersionsSubDeps() throws SAXException, ModelParseException {
+        var mock = new MockRepository("graph1");
+        var r = new Resolve(mock)
+                .addDependency(new Dependency("jresolve.test", "first.parent.of.old.dep", "1.0.0"))
+                .addDependency(new Dependency("jresolve.test", "first.parent.of.new.dep", "1.12.3"));
+        r.run();
+        assertEquals(Set.of(
+                new Dependency("jresolve.test", "first.parent.of.new.dep", "1.12.3"),
+                new Dependency("jresolve.test", "first.parent.of.old.dep", "1.0.0"),
+                new Dependency("jresolve.test", "child.new.dep", "3.4.5")
+        ), mock.downloaded);
+    }
 }
