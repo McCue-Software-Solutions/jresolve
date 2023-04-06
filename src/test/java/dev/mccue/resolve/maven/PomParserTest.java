0644c54435cf4f3c14e4d1451604889341732b0e
camperdue42 <camperdue42@gmail.com>
2023-04-06 16:05:22 -0400

    test that pomParser correctly parses dependencyManagement section

--- src/test/java/dev/mccue/resolve/maven/PomParserTest.java
+++ src/test/java/dev/mccue/resolve/maven/PomParserTest.java
@@ -14,66 +14,83 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class PomParserTest {
 
-    @Test
-    public void parseBasicPOM() throws SAXException {
-        var repository = new MockRepository("parserTest");
-        try {
-            var project = PomParser.parsePom(repository.getPom(new Dependency("dev.mccue", "resolve", "0.0.1")));
+        @Test
+        public void parseBasicPOM() throws SAXException {
+                var repository = new MockRepository("parserTest");
+                try {
+                        var project = PomParser.parsePom(repository
+                                        .getPom(new Dependency("dev.mccue", "resolve", "0.0.1")));
 
-            assertEquals(new GroupId("dev.mccue"), project.module().groupId());
-            assertEquals(new ArtifactId("resolve"), project.module().artifactId());
-            assertEquals("0.0.1", project.version());
-            assertEquals(Map.of( "junit.version.two", "5.9", "junit.version.one", "5.9.0", "pomInfo.build.sourceEncoding", "UTF-8"), project.properties());
-            assertEquals(Optional.of(Type.JAR), project.packagingOpt());
+                        assertEquals(new GroupId("dev.mccue"), project.module().groupId());
+                        assertEquals(new ArtifactId("resolve"), project.module().artifactId());
+                        assertEquals("0.0.1", project.version());
+                        assertEquals(Map.of("junit.version.two", "5.9", "junit.version.one",
+                                        "5.9.0", "pomInfo.build.sourceEncoding", "UTF-8"),
+                                        project.properties());
+                        assertEquals(Optional.of(Type.JAR), project.packagingOpt());
 
-            assertEquals(List.of(
-                    new Tuple2<>(Configuration.TEST, new Dependency(
-                            new Library(
-                                    new GroupId("org.junit.jupiter"),
-                                    new ArtifactId("junit-jupiter-api")
-                            ),
-                            "5.9.0"
-                    )),
-                    new Tuple2<>(Configuration.TEST, new Dependency(
-                            new Library(
-                                    new GroupId("org.junit.jupiter"),
-                                    new ArtifactId("junit-jupiter-params")
-                            ),
-                            "5.9.0"
-                    ))
-            ), project.dependencies());
-        } catch (ModelParseException e) { }
-    }
-
-    @Test
-    public void variableReplacementTest() throws SAXException {
-        var repository = new MockRepository("parserTest");
-
-        try {
-            var project = PomParser.parsePom(repository.getPom(new Dependency("dev.mccue", "resolve", "0.0.2")));
-            assertEquals(new GroupId("dev.mccue"), project.module().groupId());
-            assertEquals(new ArtifactId("resolve"), project.module().artifactId());
-            assertEquals("0.0.2", project.version());
-            assertEquals(Map.of( "junit.version.two", "5.9", "junit.version.one", "5.9.0", "pomInfo.build.sourceEncoding", "UTF-8"), project.properties());
-            assertEquals(Optional.of(Type.JAR), project.packagingOpt());
-
-            assertEquals(List.of(
-                    new Tuple2<>(Configuration.TEST, new Dependency(
-                            new Library(
-                                    new GroupId("org.junit.jupiter"),
-                                    new ArtifactId("junit-jupiter-api")
-                            ),
-                            "5.9.0"
-                    )),
-                    new Tuple2<>(Configuration.TEST, new Dependency(
-                            new Library(
-                                    new GroupId("org.junit.jupiter"),
-                                    new ArtifactId("junit-jupiter-params")
-                            ),
-                            "5.9.0"
-                    ))
-            ), project.dependencies());
-        } catch (ModelParseException e) {
+                        assertEquals(List.of(new Tuple2<>(Configuration.TEST,
+                                        new Dependency(new Library(new GroupId("org.junit.jupiter"),
+                                                        new ArtifactId("junit-jupiter-api")),
+                                                        "5.9.0")),
+                                        new Tuple2<>(Configuration.TEST, new Dependency(new Library(
+                                                        new GroupId("org.junit.jupiter"),
+                                                        new ArtifactId("junit-jupiter-params")),
+                                                        "5.9.0"))),
+                                        project.dependencies());
+                } catch (ModelParseException e) {
+                }
         }
-    }
+
+        @Test
+        public void variableReplacementTest() throws SAXException {
+                var repository = new MockRepository("parserTest");
+
+                try {
+                        var project = PomParser.parsePom(repository
+                                        .getPom(new Dependency("dev.mccue", "resolve", "0.0.2")));
+                        assertEquals(new GroupId("dev.mccue"), project.module().groupId());
+                        assertEquals(new ArtifactId("resolve"), project.module().artifactId());
+                        assertEquals("0.0.2", project.version());
+                        assertEquals(Map.of("junit.version.two", "5.9", "junit.version.one",
+                                        "5.9.0", "pomInfo.build.sourceEncoding", "UTF-8"),
+                                        project.properties());
+                        assertEquals(Optional.of(Type.JAR), project.packagingOpt());
+
+                        assertEquals(List.of(new Tuple2<>(Configuration.TEST,
+                                        new Dependency(new Library(new GroupId("org.junit.jupiter"),
+                                                        new ArtifactId("junit-jupiter-api")),
+                                                        "5.9.0")),
+                                        new Tuple2<>(Configuration.TEST, new Dependency(new Library(
+                                                        new GroupId("org.junit.jupiter"),
+                                                        new ArtifactId("junit-jupiter-params")),
+                                                        "5.9.0"))),
+                                        project.dependencies());
+                } catch (ModelParseException e) {
+                }
+        }
+
+        @Test
+        public void dependencyManagementTest() {
+                var repository = new MockRepository("parserTest");
+                try {
+                        var project = PomParser
+                                        .parsePom(repository.getPom(new Dependency("dev.mccue",
+                                                        "dependency.management", "0.0.1")));
+                        assertEquals(List.of(new Tuple2<>(Configuration.TEST,
+                                        new Dependency(new Library(new GroupId("org.junit.jupiter"),
+                                                        new ArtifactId("junit-jupiter-api")),
+                                                        "5.9.0")),
+                                        new Tuple2<>(Configuration.TEST, new Dependency(new Library(
+                                                        new GroupId("org.junit.jupiter"),
+                                                        new ArtifactId("junit-jupiter-params")),
+                                                        "5.9.0"))),
+                                project.dependencyManagement());
+
+
+                } catch (ModelParseException e) {
+                } catch (SAXException e) {
+                }
+
+        }
 }
