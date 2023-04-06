3f0e8d74aaa534dfeef480537643e9d856e5c138
camperdue42 <camperdue42@gmail.com>
2023-04-06 16:30:56 -0400

    add test for dependencyManagement to pomInfo

--- src/test/java/dev/mccue/resolve/maven/PomInfoTest.java
+++ src/test/java/dev/mccue/resolve/maven/PomInfoTest.java
@@ -83,14 +83,6 @@ public class PomInfoTest {
                                                         "test.pom", "0.0.1")))
                                         .toProject(repository);
 
-                        assertEquals(new GroupId("dev.test"), project.module().groupId());
-                        assertEquals(new ArtifactId("test.pom"), project.module().artifactId());
-                        assertEquals(Map.of("project.build.sourceEncoding", "UTF-8", "test.version",
-                                        "1.0.0", "junit.version.one", "5.9.0", "junit.version.two",
-                                        "5.9"), project.properties());
-                        assertEquals("0.0.1", project.version());
-                        assertEquals(Optional.of(Type.JAR), project.packagingOpt());
-
                         assertEquals(List.of(new Tuple2<>(Configuration.TEST,
                                         new Dependency(new Library(new GroupId("org.junit.jupiter"),
                                                         new ArtifactId("junit-jupiter-api")),
